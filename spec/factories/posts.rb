b902dfa33192ae4c30dec261af077ce6fb47b004
shibaei <121368277+shibaei@users.noreply.github.com>
2023-06-04 22:10:59 +0900

    post factory_bot作成

---
+++ spec/factories/posts.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :post do
+    title {Faker::Lorem.sentence}
+    place_id { Faker::Number.between(from: 2, to: 49) }
+    hotel {Faker::Company.name}
+    room { 'てすと' }
+    check_in { Faker::Date.in_date_period}
+    check_out{ Faker::Date.in_date_period }
+    text{Faker::Lorem.sentence}
+    association :user
+  end
+end
