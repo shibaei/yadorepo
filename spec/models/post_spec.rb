81daaa81f6c5db446c8ffbc30ed5b9dedcc08a34
shibaei <121368277+shibaei@users.noreply.github.com>
2023-06-08 21:59:03 +0900

    post単体テスト（画像）

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = FactoryBot.build(:post)
+  end
+
+  describe "バリデーション" do
+    it "必要な属性が揃っている場合、有効であること" do
+      expect(@post).to be_valid
+    end
+
+    it "画像がない場合、無効であること" do
+      @post.images = nil
+      expect(@post).to be_invalid
+      expect(@post.errors[:images]).to include("は1枚以上10枚以下にしてください")
+    end
+
+    it "11枚以上の画像がある場合、無効であること" do
+      11.times do
+        @post.images.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+      end
+      expect(@post).to be_invalid
+      expect(@post.errors[:images]).to include("は1枚以上10枚以下にしてください")
+    end
+  end
 end
